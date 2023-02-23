704637896667a60a9cd123301a2c975c3ff33abe
ThibaultLl <45203845+ThibaultLl@users.noreply.github.com>
2023-02-23 10:25:28 +0100

    publish

---
+++ README.md
@@ -0,0 +1,4 @@
+# Cartes de choix
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/thibaultll/pen/OJoNxmr](https://codepen.io/thibaultll/pen/OJoNxmr).
+
---
+++ script.js
@@ -0,0 +1,17 @@
+var texte1 = document.getElementById("texte1");
+var texte2 = document.getElementById("texte2");
+var texte3 = document.getElementById("texte3");
+var input1 = document.getElementById("pour_carte1");
+var input2 = document.getElementById("pour_carte2");
+var input3 = document.getElementById("pour_carte3");
+const formulaire = document.querySelector('.formulaire');
+
+function valider_formulaire() {
+  texte1.innerHTML = input1.value;
+  texte2.innerHTML = input2.value;
+  texte3.innerHTML = input3.value;
+  setTimeout(() => {
+      formulaire.style.display = 'None'; 
+    }, 400);
+  
+}
